635ec8363767d8fe1f9629d22e9f1a3bb6038658
drazil58 <drazil5826@gmail.com>
2021-01-07 17:08:41 +0530

    initial commit

---
+++ todo/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+   #Auth
+   path('signup/', views.signupuser, name='signupuser'),
+   path('current/', views.currenttodos, name='currenttodos'),
+   path('logout/', views.logoutuser, name='logoutuser'),
+   path('login/', views.loginuser, name='loginuser'),
+   path('', views.home, name='home'),
+   path('create/', views.createtodo, name='createtodo'),
+   path('completed/', views.completed, name='completed'),
+   path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+   path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+   path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+]
