0366581a583f9e1ac12e6dc3ac12c4b7cff5b3ed
azizsharif <azizsharif2000@gmail.com>
2021-06-18 10:51:46 +0000

    Remove old stuff

--- Todowoo/settings.py
+++ Todowoo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p&i!s7$1__r5abz2)v0v=1_!km+wo6h7(_&+x@pdlmhyqjv35v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['azizsharif.pythonanywhere.com']
 
 
 # Application definition
@@ -119,10 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_URL = '/login/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
